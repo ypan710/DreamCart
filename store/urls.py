1254569e41a43e91c7dc9f3265d557bf3dabaea9
Yongjian Pan <46613683+ypan710@users.noreply.github.com>
2023-10-04 22:48:34 -0700

    First commit

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/',
+         views.product_detail, name='product_detail'),
+]
