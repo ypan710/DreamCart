ca4d964a172558d764625396ec69ab17fc64301c
Yongjian Pan <46613683+ypan710@users.noreply.github.com>
2023-10-23 11:24:17 -0700

    Add forms and url and templates for accounts application

--- dreamkart/settings.py
+++ dreamkart/settings.py
@@ -134,3 +134,18 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+
+# SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'y439296@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
